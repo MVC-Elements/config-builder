8ba85217029b59fd0ab065f450df935e432768b3
Stepan Suvorov <stevermeister@gmail.com>
2015-03-07 18:22:05 +0100

    remove old code

--- server.js
+++ server.js
@@ -4,17 +4,10 @@ var app = express();
 app.use(express.static(__dirname + '/public'));
 app.use(express.bodyParser());
 
-createApi(app, config.devMode, {
-  dbclient: dbclient,
-  clientId: config.clientId,
-  clientSecret: config.clientSecret
-});
-
 //static content
 app.all('/*', function(req, res) {
   res.sendfile('index.html', { root: __dirname+'/public' });
 });
-
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
